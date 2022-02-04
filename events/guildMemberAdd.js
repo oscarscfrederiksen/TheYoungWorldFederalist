d616f949925eb29b2ff644adb83b0406179cd9f7
Oscar Frederiksen <oscar.frederiksen@me.com>
2022-02-04 20:21:14 +0000

    Added variation to greetings

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,11 +1,13 @@
 const {serverID} = require("../config.json");
+const shortGreetings = ["Hello", "Hey", "Howdy", "Hello there", "Hey there", "Hej", "Bonjour", "Hi", "Greetings", "Welcome", "Hola", "Ciao", "Hallo", "Olá", "Siemano", "Merhaba", "Goededag", "Salve", "Szervusvz", "Terve", "Ahoj"]
+const longGreetings = ["glad you found us", "nice to meet you", "happy you are here", "thanks for joining", "great to have you here", ""]
 
 module.exports = {
     name: 'guildMemberAdd',
     execute(member, bot) {
         userNumber = bot.guilds.cache.get(serverID).memberCount
         console.log('User ' + member.user.username + ' has joined the server!');
-        member.guild.channels.cache.find(c => c.name === "welcome").send(`Hello <@${member.user.id}>, glad you found us!\nYou are our ${ordinal(userNumber)} member :partying_face:\n\n **Answer these three questions in #Introductions:**\n:round_pushpin: Where are you from? (to put you in contact with your local section)\n:mag_right: How did you find the server?\n:globe_with_meridians: Have you heard of world federalism? If so, what do you think?`)
+        member.guild.channels.cache.find(c => c.name === "welcome").send(`${shortGreetings[Math.floor(Math.random() * shortGreetings.length)]} <@${member.user.id}>, ${longGreetings[Math.floor(Math.random() * longGreetings.length)]}!\nYou are our ${ordinal(userNumber)} member :partying_face:\n\n **Answer these three questions in #Introductions:**\n:round_pushpin: Where are you from? (to put you in contact with your local section)\n:mag_right: How did you find the server?\n:globe_with_meridians: Have you heard of world federalism? If so, what do you think?`)
     }
 }
 
