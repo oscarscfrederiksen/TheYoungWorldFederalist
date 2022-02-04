60aaf772084ae39077e215310b28713dd79f6af2
Oscar Frederiksen <oscar.frederiksen@me.com>
2022-02-04 21:19:09 +0000

    Improved formatting

--- commands/utils/embed.js
+++ commands/utils/embed.js
@@ -1,15 +1,19 @@
 const { MessageEmbed } = require("discord.js");
 const colors = ['#c84639', '#eeb747', '#8bb149', '#2f5e9e']
+
 exports.run = async (bot, message, args) => {
     let fullArgs = args.join(" ")
+
     var title = fullArgs.substring(
         fullArgs.split('"', 1).join('"').length + 1,
         fullArgs.split('"', 2).join('"').length
     );
+
     var description = fullArgs.substring(
         fullArgs.split('"', 3).join('"').length + 1,
         fullArgs.split('"', 4).join('"').length
-    )
+    );
+    
     const embed = new MessageEmbed()
 	    .setColor(`${colors[Math.floor(Math.random() * colors.length)]}`)
 	    .setTitle(`${title}`)
