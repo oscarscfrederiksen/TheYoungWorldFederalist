7a20faf2b26ffd8e98aa1a96b80c4e22d8ff2c57
Oscar Frederiksen <oscar.frederiksen@me.com>
2022-02-16 16:00:22 +0100

    Added further edge cases for purge.

--- interactions/slash/admin/purge.js
+++ interactions/slash/admin/purge.js
@@ -14,10 +14,12 @@ module.exports = {
     ),
     
     async execute(interaction) {
+      process.on('unhandledRejection', (error) => {
+        interaction.reply(`${error.toString().replace("DiscordAPIError: ", "")}`)
+      });
       const numberOfMessages = interaction.options.getInteger("number")
       if (!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.reply("**You do not have the permissions required for this command! - [MANAGE_MESSAGES]**")
       if (numberOfMessages < 1 || numberOfMessages > 100) return interaction.reply("**Please supply a number of messages between 1 and 100.**")
-      interaction.channel.bulkDelete(numberOfMessages)
-        .then(messages => interaction.reply(`**Successfully deleted \`${messages.size}/${numberOfMessages}\` messages**`).then(msg => msg.delete({timeout:5000}))).catch(err => console.log(err))
+      interaction.channel.bulkDelete(numberOfMessages).then(messages => interaction.reply(`**Successfully deleted \`${messages.size}/${numberOfMessages}\` messages**`))
     }
 }
