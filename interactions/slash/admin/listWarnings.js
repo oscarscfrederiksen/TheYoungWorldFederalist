6bd013d04a31a32b1d81ea96b3b18cd2885e7c49
Oscar Frederiksen <oscar.frederiksen@me.com>
2022-02-18 20:47:10 +0100

    Allow more time for response from database.

--- interactions/slash/admin/listWarnings.js
+++ interactions/slash/admin/listWarnings.js
@@ -15,58 +15,60 @@ module.exports = {
         ),
     
     async execute(interaction) {
-      const guildId = interaction.guild.id
-      const warnUser = interaction.options.getUser("user")
-      const userId = warnUser.id
+        const fetchWarnings = async () => {
+            const guildId = interaction.guild.id
+            const warnUser = interaction.options.getUser("user")
+            const userId = warnUser.id
 
-      let numberOfWarnings = 0;
-      var logDescription;
-      let warningList;
+            let numberOfWarnings = 0;
+            var logDescription;
+            let warningList;
 
-      if (!interaction.member.permissions.has("KICK_MEMBERS")) return interaction.reply("**You don't have the permission to view warns! - [MANAGE_MEMBERS]**")
-      if (!warnUser) return interaction.reply("**This member doesn't seem to exist...**")
+            if (!interaction.member.permissions.has("KICK_MEMBERS")) return interaction.editReply("**You don't have the permission to view warns! - [MANAGE_MEMBERS]**")
+            if (!warnUser) return interaction.editReply("**This member doesn't seem to exist...**")
 
-      await mongoConnection().then(async (mongoose) => {
-          try {
-              const results = await warnSchema.findOne({
-                  guildId,
-                  userId,
-              })
+            await mongoConnection().then(async (mongoose) => {
+                try {
+                    const results = await warnSchema.findOne({
+                        guildId,
+                        userId,
+                    })
 
-              warningList = `\n`
-              try {
-                for (const warning of results.warnings) {
-                    numberOfWarnings += 1
-                    const {author, timestamp, reason } = warning
-                    warningList += `${numberOfWarnings}. By ${author} on ${timestamp}. ${reason}\n`
+                    warningList = `\n`
+
+                    try {
+                        for (const warning of results.warnings) {
+                            numberOfWarnings += 1
+                            const {author, timestamp, reason } = warning
+                            warningList += `${numberOfWarnings}. By ${author} on ${timestamp}. ${reason}\n`
+                        }
+                    } catch {
+                        // There are no warnings.
+                    }
+                } finally {
+                    mongoose.connection.close()
                 }
-              } catch {
-                // There are no warnings.
-              }
+            })
 
-          } finally {
-              mongoose.connection.close()
-          }
-      })
+            if (numberOfWarnings > 1) {
+                logDescription = `${warnUser.username} has been warned ${numberOfWarnings} times.\n${warningList}`
+            } else if (numberOfWarnings == 1) {
+                logDescription = `${warnUser.username} has been warned ${numberOfWarnings} time.\n${warningList}`
+            } else {
+                logDescription = `${warnUser.username} has not previously been warned.`
+            }
 
-      if (numberOfWarnings > 1) {
-          logDescription = `${warnUser.username} has been warned ${numberOfWarnings} times.\n${warningList}`
-      } else if (numberOfWarnings == 1) {
-          logDescription = `${warnUser.username} has been warned ${numberOfWarnings} time.\n${warningList}`
-      } else {
-          logDescription = `${warnUser.username} has not previously been warned.`
-      }
-
-      const logEmbed = new MessageEmbed()
-            .setAuthor(`Warning list for ${warnUser.username}`, interaction.guild.iconURL())
-            .setColor("#c84639")
-            .setThumbnail(warnUser.displayAvatarURL({ dynamic: true }))
-            .setDescription(`${logDescription}`)
-            .setTimestamp()
-        
-      interaction.reply(
-            { embeds : [logEmbed] }
-      )
-
+            const logEmbed = new MessageEmbed()
+                .setAuthor(`Warning list for ${warnUser.username}`, interaction.guild.iconURL())
+                .setColor("#c84639")
+                .setThumbnail(warnUser.displayAvatarURL({ dynamic: true }))
+                .setDescription(`${logDescription}`)
+                .setTimestamp()
+            
+            return logEmbed
+        }
+        interaction.deferReply()
+        const result = await fetchWarnings()
+        interaction.editReply({embeds : [result]})
     }
 }
